5995bcc47bb7f045c024b32a2f034a017071843d
para7 <cometpara7@gmail.com>
2024-07-09 15:57:45 +0900

    サーバーサイドでコンポーネントフェッチしないよう修正

--- src/routes/proto/api/+page.svelte
+++ src/routes/proto/api/+page.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
 	import { honoClient } from "$lib/hono/honoClient";
 	import type { PageData } from "./$types";
+	import { svelte_client_fetch } from "$lib/utilities/svelte/svelte-fetch";
 
 	export let data: PageData;
 
-	let vali = honoClient.api.validate
-		.$post({ json: { id: "id" } })
-		.then(async (x) => {
-			if (x.ok) {
-				const sss = await x.json();
-				console.log(sss);
-				return sss;
-			} else {
-				console.error(x);
-				return { ...x, status: x.status, json: await x.json().catch((e) => String(e)) };
-			}
-		})
-		.catch(async (e) => {
-			console.error(e);
-		});
+	let vali = svelte_client_fetch(() => {
+		return honoClient.api.validate
+			.$post({ json: { id: "id" } })
+			.then(async (x) => {
+				if (x.ok) {
+					const sss = await x.json();
+					console.log(sss);
+					return sss;
+				} else {
+					console.error(x);
+					return { ...x, status: x.status, json: await x.json().catch((e) => String(e)) };
+				}
+			})
+			.catch(async (e) => {
+				console.error(e);
+			});
+	});
 </script>
 
 <div>【data】</div>
@@ -29,6 +32,9 @@
 {:then res}
 	<div>【vali】</div>
 	<div class="pre">{JSON.stringify(res, null, "\t")}</div>
+{:catch e}
+	<div>error</div>
+	<pre>{e}</pre>
 {/await}
 
 <style>
